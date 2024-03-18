869aa86930091ab8041010aa24a0c3ec85235f11
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-03-18 23:05:14 +0900

    feat(nvim-minimal): Add nvim config with lua - minimal version
    BREAKING CHANGE:

---
+++ nvim-minimal/lua/plugins/solarized-osaka.lua
@@ -0,0 +1,11 @@
+return {
+  "craftzdog/solarized-osaka.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = function()
+    vim.cmd[[colorscheme solarized-osaka]]
+    return {
+      transparent = true,
+    }
+  end
+}
