4762c99858ee63544fd6c54386825a9c16c003a6
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2025-01-24 12:40:15 +0900

    add cyberdream theme in nvim

---
+++ cyberdream.lua
@@ -0,0 +1,13 @@
+return {
+  "scottmckendry/cyberdream.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("cyberdream").setup({
+      transparent = true,
+      transparent_bg = true,
+    })
+    vim.cmd[[colorscheme cyberdream]]
+  end
+}
+
